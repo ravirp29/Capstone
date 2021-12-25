19f768b5e602b8ed98da3692adb71b2e847dec66
ravirp29 <36395546+ravirp29@users.noreply.github.com>
2021-12-25 22:04:15 +0530

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/layout/Header';
+import MusicPanel from './components/MusicPanel';
+
+import './css/style.css'
+
+const App = () => {
+	return (
+		<div className="container">
+			<Header />
+			<MusicPanel />
+		</div>
+	);
+};
+
+
+
+export default App;
